95d3a1cbb21f72f7780531cff88823de54b610de
Jungho Cho <christopher@postech.ac.kr>
2021-05-09 08:56:08 +0900

    Initial commit Status calculator implemented Status GUI implemented Weapon GUI in implementation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SakiSimulatorGUI
-Simulator of the mobile game "Sasin Kiugi" with GUI support
+This project is originated from the [사키 시뮬레이터](https://cafe.naver.com/rookieonline?iframe_url_utf8=%2FArticleRead.nhn%3FreferrerAllArticles%3Dtrue%26page%3D1%26searchBy%3D1%26query%3D%25EC%2598%2581%25EC%2596%25B4%26exclude%3D%26include%3D%26exact%3D%26searchdate%3Dall%26media%3D0%26sortBy%3Ddate%26clubid%3D30211687%26articleid%3D65462).
+
---
+++ src/weapon_page.py
@@ -0,0 +1,164 @@
+import tkinter as tk
+import tkinter.font as tkf
+import PIL
+import re
+
+from PIL import ImageTk, Image
+from utils import *
+
+class WeaponPage(tk.Frame):
+    GRID_POS = {
+        '저급': (0, 1),
+        '일반': (0, 6),
+        '고급': (0, 11),
+        '영웅': (7, 1),
+        '전설': (7, 6),
+        '신': (18, 1),
+        '불멸': (18, 6)
+    }
+    def __init__(self, master):
+        super().__init__(bg='white')
+
+        img_size = (90, 90)
+
+        self.imgs = {}
+        for key in WeaponPage.GRID_POS.keys():
+            self.imgs[key] = []
+            for i in reversed(range(1, 5)):
+                img = PIL.Image.open('data/img/weapon/%s%d.png'%(key, i)).resize(img_size, PIL.Image.ANTIALIAS)
+                photo = ImageTk.PhotoImage(img)
+                self.imgs[key].append(photo)
+
+        self.levels = {}
+        for key in WeaponPage.GRID_POS.keys():
+            self.levels[key] = []
+            for i in range(4):
+                self.levels[key].append(tk.StringVar())
+
+        self.req_ups = {}
+        for key in WeaponPage.GRID_POS.keys():
+            self.req_ups[key] = []
+            for i in range(4):
+                self.req_ups[key].append(tk.StringVar())
+
+        self.full_req_ups = {}
+        for key in WeaponPage.GRID_POS.keys():
+            self.full_req_ups[key] = []
+            for i in range(4):
+                self.full_req_ups[key].append(tk.StringVar())
+
+        self.mount_effect = {}
+        for key in WeaponPage.GRID_POS.keys():
+            self.mount_effect[key] = []
+            for i in range(4):
+                self.mount_effect[key].append(tk.StringVar())
+
+        self._render_frame()
+
+    def _update_userdata(self, master):
+        pass
+
+    def _render_frame(self):
+        self._render_context()
+        self._render_image()
+        self._render_level_slot()
+        self._render_needed_upstone()
+        self._render_mount_effect()
+    
+    def _render_context(self):
+        label_texts = ['레벨 (미 보유시 0)', '현재 필요한 업스톤', '풀업글 시 필요 업스톤', '현재 장착효과', '현재 보유효과', '개당 업스톤 업그레이드 효율']
+        for i, t in enumerate(label_texts):
+            label = tk.Label(self, text=t, font=tkf.Font(family="Maplestory", size=10), bg='white', fg="#202020")
+            label.grid(row=i+1, column=0, sticky='e')
+
+        label_texts = ['레벨 (미 보유시 0)', '현재 필요한 업스톤', '풀업글 시 필요 업스톤', '현재 장착효과', '장착 시 치명타 확률', 'HP/MP/회피/회복 증가율', '현재 보유효과', '개당 업스톤 업그레이드 효율']
+        for i, t in enumerate(label_texts):
+            label = tk.Label(self, text=t, font=tkf.Font(family="Maplestory", size=10), bg='white', fg="#202020")
+            label.grid(row=i+8, column=0, sticky='e')
+
+        label_texts = ['레벨 (미 보유시 0)', '현재 필요한 업스톤', '풀업글 시 필요 업스톤', '현재 장착효과', '장착 시 치명타 확률', '장착 시 치명타 피해 증가율', '회피/드랍/물공/마공 증가율', '현재 보유효과', '개당 업스톤 업그레이드 효율', '보유2 공/마 업그레이드 효율']
+        for i, t in enumerate(label_texts):
+            label = tk.Label(self, text=t, font=tkf.Font(family="Maplestory", size=10), bg='white', fg="#202020")
+            label.grid(row=i+19, column=0, sticky='e')
+
+    def _render_level_slot(self):
+        for key in WeaponPage.GRID_POS.keys():
+            for i in range(4):
+                vcmd = (self.register(self._validate_level), '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
+                entry = tk.Entry(self, width=10, textvariable=self.levels[key][i], font=tkf.Font(family="Maplestory", size=10),
+                                    justify='right', validate = 'key', validatecommand = vcmd)
+                entry.bind('<Return>', self._update_value)
+                entry.bind('<FocusOut>', self._update_value)
+                r_offset, c_offset = WeaponPage.GRID_POS[key]
+                entry.grid(row=r_offset+1, column=c_offset + i, sticky='we')
+
+    def _render_needed_upstone(self):
+        for key in WeaponPage.GRID_POS.keys():
+            for i in range(4):
+                label = tk.Label(self, textvariable=self.req_ups[key][i], font=tkf.Font(family="Maplestory", size=10), bg='white', fg="#202020")
+                r_offset, c_offset = WeaponPage.GRID_POS[key]
+                label.grid(row=r_offset+2, column=c_offset + i, sticky='e')
+
+        for key in WeaponPage.GRID_POS.keys():
+            for i in range(4):
+                label = tk.Label(self, textvariable=self.full_req_ups[key][i], font=tkf.Font(family="Maplestory", size=10), bg='white', fg="#202020")
+                r_offset, c_offset = WeaponPage.GRID_POS[key]
+                label.grid(row=r_offset+3, column=c_offset + i, sticky='e')
+
+    def _render_mount_effect(self):
+        for key in WeaponPage.GRID_POS.keys():
+            for i in range(4):
+                label = tk.Label(self, textvariable=self.mount_effect[key][i], font=tkf.Font(family="Maplestory", size=10), bg='white', fg="#202020")
+                r_offset, c_offset = WeaponPage.GRID_POS[key]
+                label.grid(row=r_offset+4, column=c_offset + i, sticky='e')
+
+    def _render_image(self):
+        for key in WeaponPage.GRID_POS.keys():
+            for i in range(4):
+                img = self.imgs[key][i]
+                label = tk.Label(self, bg='white', image=img)
+                r_offset, c_offset = WeaponPage.GRID_POS[key]
+                label.grid(row=r_offset, column=c_offset + i)
+
+    def _validate_level(self, action, index, value_if_allowed,
+                    prior_value, text, validation_type, trigger_type, widget_name):
+        only_digit = re.match(r"^[0-9]*$", value_if_allowed) is not None
+        if not only_digit:
+            return False
+
+        if value_if_allowed:
+            val = int(value_if_allowed)
+            right_range = val in range(201)
+
+            return right_range
+        else:
+            return True
+
+    def _update_value(self, event):
+        for key in WeaponPage.GRID_POS.keys():
+            for i in range(4):
+                level = self.levels[key][i].get()
+                level = int(level) if level else 0
+                self.req_ups[key][i].set(
+                    transform_english_amount_string(
+                        calc_needed_upstone(key, level)
+                    )
+                )
+                self.full_req_ups[key][i].set(
+                    transform_english_amount_string(
+                        calc_full_needed_upstone(key, level)
+                    )
+                )
+                weapon_effect = calc_weapon_effect(key, i, level)
+                self.mount_effect[key][i].set(
+                    transform_english_amount_string(
+                        weapon_effect['공격력']
+                    ) + '%'
+                )
+        pass
+
+
+    
+
+
+        
